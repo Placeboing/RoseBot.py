2a65980521378cafb8d101ee65f54772ed9c7cbb
Immy <43959514+Placeboing@users.noreply.github.com>
2020-10-13 14:42:15 +0100

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,14 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
 
 PREFIX = "rpy "
 OWNER_IDS = [619595472733995009]
+
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
