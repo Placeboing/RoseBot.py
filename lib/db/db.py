5a4d7afce326976dce4e41317267ee2230dd0921
Immy <43959514+Placeboing@users.noreply.github.com>
2020-10-13 14:09:55 +0100

    Added files

--- lib/db/db.py
+++ lib/db/db.py
@@ -29,6 +29,7 @@ def commit():
 def close():
 	cxn.close()
 
+
 def field(command, *values):
 	cur.execute(command, tuple(values))
 
@@ -52,3 +53,16 @@ def column(command, *values):
 	cur.execute(command, tuple(values))
 
 	return [item[0] for item in cur.fetchall()]
+
+
+def execute(command, *values):
+	cur.execute(command, tuple(values))
+
+
+def multiexec(command, valueset):
+	cur.executemany(command, valueset)
+
+
+def scriptexec(path):
+	with open(path, "r", encoding="utf-8") as script:
+		cur.executescript(script.read())
